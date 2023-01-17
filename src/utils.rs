c507f5b6ea3bd57aea7175efbcbec34fe2a8ff29
Thomas Ferdinand <thomasferdinand96@gmail.com>
2023-01-16 22:50:53 -0500

    aquire target done-ish

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,9 @@
     @TODO: rename to globals.rs or global.rs ?
 */
 
-use bevy::{app::PluginGroupBuilder, prelude::*};
+use bevy::{prelude::*, utils::FloatOrd};
+
+use crate::GameState;
 
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)]
@@ -30,7 +32,12 @@ impl Plugin for UtilsPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<Health>()
             .register_type::<Range>()
-            .register_type::<AttackCooldown>();
+            .register_type::<AttackCooldown>()
+            .add_system_set(
+                SystemSet::on_update(GameState::Gameplay)
+                    .with_system(tick_timers)
+                    .with_system(aquire_target),
+            );
     }
 }
 
@@ -40,14 +47,48 @@ fn tick_timers(mut timers: Query<&mut AttackCooldown>, time: Res<Time>) {
     }
 }
 
+// find closest target in range
+// maybe need to add With<PlayerOwned>, or maybe not so enemies can use this too
+// still would need two diff teams components to tell who enemies are
+// @TODO: make this have less calculations, (try to capture and reuse values)
 fn aquire_target(
     mut commands: Commands,
-    shooter: Query<Entity, (With<Range>, Without<Target>)>,
-    targets: Query<Entity, With<Health>>,
+    shooters: Query<(Entity, &Range, &GlobalTransform), Without<Target>>,
+    targets: Query<(Entity, &GlobalTransform), With<Health>>, // @TODO: With<EnemyOwned>
 ) {
+    for (shooter_ent, range, shooter_transform) in shooters.iter() {
+        let closest_enemy = targets
+            .iter()
+            .min_by_key(|closest_transform| {
+                FloatOrd(Vec3::distance(
+                    shooter_transform.translation(),
+                    closest_transform.1.translation(),
+                ))
+            })
+            .map(|target_in_range| target_in_range); //@TODO: dont think i need this?
+
+        if let Some(closest_enemy) = closest_enemy {
+            let distance = Vec3::distance(
+                closest_enemy.1.translation(),
+                shooter_transform.translation(),
+            );
+
+            if distance.abs() <= range.0 as f32 {
+                commands.entity(shooter_ent).insert(Target {
+                    target: closest_enemy.0,
+                });
+
+                info!(
+                    "Entity: {:?} is targeting entity: {:?}",
+                    shooter_ent, closest_enemy.0
+                );
+            }
+        }
+    }
 }
 
 // if target moves out of range, remove the target
 fn remove_target() {}
 
+// for entities with a target, shoot them
 fn attack_target() {}
